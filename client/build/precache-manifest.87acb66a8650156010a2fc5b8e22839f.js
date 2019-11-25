c917b20086a3d3d576f4089a3a302c6d6f0f3b04
Hugo Arvis <arvishugo@hotmail.fr>
2019-11-25 12:08:57 +0100

    little change

---
+++ client/build/precache-manifest.87acb66a8650156010a2fc5b8e22839f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "854642180fc71b83f96a3303910a91c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8004d041eac743ceacf7",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "7e5ff9b49f2c47036219",
+    "url": "/static/css/main.cd9963a9.chunk.css"
+  },
+  {
+    "revision": "8004d041eac743ceacf7",
+    "url": "/static/js/2.59ed3a23.chunk.js"
+  },
+  {
+    "revision": "7e5ff9b49f2c47036219",
+    "url": "/static/js/main.c854dcc6.chunk.js"
+  },
+  {
+    "revision": "149913179df51d32bfcc",
+    "url": "/static/js/runtime-main.0b6963e1.js"
+  },
+  {
+    "revision": "c7bae4807b97413d696044064f9cf9ff",
+    "url": "/static/media/logorugby.c7bae480.png"
+  }
+]);
